735eedc92f4023abb9002b6d916c76f1c1ae480d
mashuyi <1763360594@qq.com>
2018-09-12 22:01:18 +0800

    2018/09/12

--- Python/rundata_read.py
+++ Python/rundata_read.py
@@ -1,24 +1,7 @@
 import os
 
 # os.chdir("D:\\01mashuyi\\DailySummary\\Python\\HeadFirstPython\\chapter5")
-os.chdir("G:\\00mashuyi\\DailySummary\\Python\\HeadFirstPython\\chapter5")
-
-# 打开文件读取数据的函数
-sarah = []
-def readData(fileName):
-	try:
-		with open(fileName) as readFile:
-			data = readFile.readline()
-		return(data.strip().split(','))
-	except IOError as ioerr:
-		print('File error:' + str(ioerr))
-		return(None)
-
-james = readData('james.txt')
-julie = readData('julie.txt')
-mikey = readData('mikey.txt')
-sarah = readData("sarah.txt")
-
+os.chdir("G:\\00mashuyi\\DailySummary\\Python\\HeadFirstPython\\chapter6")
 
 # 函数用来过滤'-'和':'不标准的字符串数据
 def sanitize(time_string):
@@ -31,12 +14,26 @@ def sanitize(time_string):
 	(mins,secs) = time_string.split(splitter)
 	return(mins + '.' +secs)
 
-jamesData = []
-julieData = []
-mikeyData = []
-sarahData = []
+# 打开文件读取数据的函数
+sarah = []
+def readData(fileName):
+	try:
+		with open(fileName) as readFile:
+			data = readFile.readline()
+			temp = data.strip().split(',')
+		return({'Name':temp.pop(0),
+			'BIrth':temp.pop(0),
+			'Times':str(sorted(set([sanitize(each) for each in temp]))[0:3])})
+	except IOError as ioerr:
+		print('File error:' + str(ioerr))
+		return(None)
 
-jamesData = print(sorted(set([sanitize(each) for each in james]))[0:3])
-julieData = print(sorted(set([sanitize(each) for each in julie]))[0:3])
-mikeyData = print(sorted(set([sanitize(each) for each in mikey]))[0:3])
-sarahData = print(sorted(set([sanitize(each) for each in sarah]))[0:3]) 
+james = readData('james2.txt')
+julie = readData('julie2.txt')
+mikey = readData('mikey2.txt')
+sarah = readData("sarah2.txt")
+print(sarah)
+print(sarah['Name'] + "'s fastest times are:" + sarah['Times'])
+print(james['Name'] + "'s fastest times are:" + james['Times'])
+print(julie['Name'] + "'s fastest times are:" + julie['Times'])
+print(mikey['Name'] + "'s fastest times are:" + mikey['Times'])
